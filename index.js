5caad6b5b77cee58ce0908c8d9c5b02d6453c3ca
rachellesg <rachelle.yeo@outlook.com>
2020-01-09 16:12:45 +0800

    cleaned code

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-process.argv.forEach(function(v) {
-    console.log(v);
-});
+// process.argv.forEach(function(v) {
+//     console.log(v);
+// });
 
 const hexToRgb = require ('./hextorgb.js');
 const rgbToHex = require ('./rgbtohex.js');
 const rgbToHsl = require ('./rgbtohsl.js');
 const hexToHsl = require ('./hextohsl.js');
 
-
 if (process.argv[2] === "hex") {
+
     console.log(hexToRgb(process.argv[3]));
     console.log(hexToHsl(process.argv[3]));
+
 } else if (process.argv[2] === "rgb") {
-    console.log(rgbToHex(parseInt(process.argv[3]),parseInt(process.argv[4]),parseInt(process.argv[5])));
-    console.log(rgbToHsl(parseInt(process.argv[3]),parseInt(process.argv[4]),parseInt(process.argv[5])));
+
+    let r = parseInt(process.argv[3]);
+    let g = parseInt(process.argv[4]);
+    let b = parseInt(process.argv[5]);
+
+    console.log(rgbToHex(r,g,b));
+    console.log(rgbToHsl(r,g,b));
 }
